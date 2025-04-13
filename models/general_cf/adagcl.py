4ddcb0ba149e28e24ba79c95286daf6e88651659
whos-soso <603811158@qq.com>
2025-04-14 01:05:15 +0800

    Update adagcl.py

--- models/general_cf/adagcl.py
+++ models/general_cf/adagcl.py
@@ -7,7 +7,7 @@ from models.loss_utils import cal_bpr_loss, reg_params, cal_infonce_loss
 import torch_sparse
 from copy import deepcopy
 import numpy as np
-
+from torch.nn.modules.module import Module
 from torch.nn.parameter import Parameter
 # from layers import GraphConvolution
 import torch.distributions as tdist
