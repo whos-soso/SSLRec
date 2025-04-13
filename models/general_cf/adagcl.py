758c805beb031d45f9e86a0f46e8147f5127eb0e
whos-soso <603811158@qq.com>
2025-04-14 01:46:15 +0800

    Update adagcl.py

--- models/general_cf/adagcl.py
+++ models/general_cf/adagcl.py
@@ -501,28 +501,17 @@ class VGAE(nn.Module):
         else:
             return mu
 
+
+
     def forward_encoder(self, adj):
-        # adj shape: [B, N, N]
-        B, N = adj.size()
-        device = adj.device
+        self.is_training = True
+        x_u, x_i = self.adagcl.forward(adj)
+        x = t.concat([x_u.detach(), x_i.detach()])
+        mu, logvar = self.encode(x.unsqueeze(0), adj)
+        z = self.reparameterize(mu, logvar)
+        return z.squeeze(0), mu.squeeze(0), logvar.squeeze(0)	
 
-        mu_all = []
-        logvar_all = []
-
-        for i in range(self.K + self.J):
-            x = torch.eye(N, device=device)
-            edge_index = adj[i].cpu().nonzero(as_tuple=False).t().contiguous().cuda()
-            mu, logvar = self.encode(x, edge_index)
-            mu_all.append(mu)
-            logvar_all.append(logvar)
-
-        mu_all = torch.stack(mu_all)      # [K+J, N, latent_dim]
-        logvar_all = torch.stack(logvar_all)
-
-        assert self.J > 0, "J must be > 0 to compute latent variables."
-        z = self.reparameterize(mu_all[self.K:], logvar_all[self.K:])  # [J, N, latent_dim]
-        return z, mu_all, logvar_all
-
+	
     def cal_loss_vgae(self, data, batch_data):
         users, items, neg_items = batch_data
         x, x_mean, x_std = self.forward_encoder(data)
